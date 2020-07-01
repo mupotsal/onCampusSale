c75a2b503c20baf4b86a8fc525474ee75f1b860c
mupotsal <46546491+mupotsal@users.noreply.github.com>
2020-06-30 22:17:26 -0400

    done

---
+++ ShopX/main/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from project.main.routes import main
+from project.admin.routes import admin
+
+app = Flask(__name__)
+
+app.register_blueprint(main, url_prefix='/')
+app.register_blueprint(admin, url_prefix='/admin')
